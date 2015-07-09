a876d872fbaabeef30a51002d211c6dcaacb1ca9
siddharthjn <siddharth.smu@gmail.com>
2015-07-09 07:53:03 -0300

    Content loaded according to circles

--- customJS/table.js
+++ customJS/table.js
@@ -1,26 +1,27 @@
-function clickCircles()
-{ 
-
 $( "#round-button-circle-Entry" ).click(function() {
-  if($("#round-button-circle-Foundation").data('clicked') || $( "#round-button-circle-Expert" ).data('clicked') ||
-  	$( "#round-button-circle-Lead" ).data('clicked'))
-  {
-  	alert("wtf!");
-  }
-  else
-  {
-  $('.table tr:last').after('<tr> <td> 2 </td> <td> ha </td> <td> ha </td> <td> ha </td> <td> ha </td> </tr>');
-  $(this).data('clicked', true);
-}
+  $('.table tbody').remove();
+  $('.table').append('<tr><td>1</td><td>Analysing a Work Item</td><td>Complete Knowledge Transfer/Job Shadowing </td><td>Attend Java Bootcamp If Aplicable</td></tr>');
+  $('.table').append('<tr><td>2</td><td>Delivering Work Items at A steady Rate</td><td>Deliver an appropriate number of work items per week -- check with your team lead for guidance</td><td>Bluemix Getting started for Architects & Developers (ILX_O_URL-ILXA-9HMRJY_11)</td>');
+  $('.table').append('<tr><td>3</td><td>Getting familiar with Scrum/AAO Methodology if applicable</td><td>Document atleast 2 complex problems on Wiki</td><td>Bluemix and the Internet of Things (ILX_O_URL-ILXA-9M5AS7_11) </td></tr>');
+  $('.table').append('<tr><td>4</td><td>Unit & Regression Testing</td><td>Identify & Document at least 5 workstation problems with solutions</td><td>Java - Introduction to Hibernate (00738587)</td></tr>');
+  $('.table').append('<tr><td>5</td><td>Documenting/Maintaining KT Docs</td><td>-</td><td>Complete 3 Analytics courses - http://w3.blueprint.sby.ibm.com/b_dir/blueprint.nsf/url/AB49344OpenDocument</td></tr>');
 });
 
 
 $( "#round-button-circle-Foundation" ).click(function() {
-  $('.table tr:last').after('<tr> <td> 2 </td> <td> ha </td> <td> ha </td> <td> ha </td> <td> ha </td> </tr>');
-  $(this).data('clicked', true);
+  /*$('.table tr:last').after('<tr> <td> 2 </td> <td> ha </td> <td> ha </td> <td> ha </td> <td> ha </td> </tr>');
+  $(this).data('clicked', true);*/
+  $('.table tbody').remove();
+  $('.table').append('<tr><td>1</td><td>Apply the best design pattern to a solution after consulting senior members</td><td>Deliver work items with the best design solution. By now a developer should be fluent with 2-3 design patterns (example factory method, singleton pattern etc)</td><td><p>Learning JavaScript Design Patterns - Addy Osmani (ILX_O_URL-ILXA-9XYS6G_11)</td></tr>');
+  $('.table').append('<tr><td>2</td><td>Accurately estimate the size/complexity of work items and the time needed to solve a work item)</td><td>Take part in sizing meeting with scrum masters/Team Lead to break down big chunk of work( epics) into smaller achievable tasks. Estimate the time taken and complexity of problem as a team</td><td>Scrum practitioner certification if applicable to the project -<p> (I) SMU Session 2: Scrum Roles and Intro to Scrum Master Role  (ILX_O_URL-ILXA-9VMMLM_11)</p> <p>(ii) Using scrum methods with Rational Team Concert Version 4: Part 2. Plan and manage sprints - developerWorks  (ILX_O_URL-ILXA-9GQKYQ_11)</p></td></tr>');
+  $('.table').append('<tr><td>3</td><td>Interpret and Deliver requirement analysis/wireframes drawn by the Business Analyst.</td><td>Deliver work Leverage the knowledge of  business analyst to understand the business requirements, application and wireframes/td><td>Training in Bluemix & 1 Technology outside the project</td></tr>');
+  $('.table').append('<tr><td>4</td><td>Presenting and communicating solution to the team & product owners via sametime meeting room, ppt or on call.</td><td>Present the solution to team lead and product owners</td><td>Programming in C++: Structure and Data Types (SSSA9Q20430150113)</td></tr>');
+  $('.table').append('<tr><td>5</td><td>Accurately estimate the size/complexity of work items and the time needed to solve a work item)</td><td>Complete the required training to understand the industry associated with project</td><td>Training courses for Web Services/ Soap/ Restful API</td></tr>');
+  $('.table').append('<tr><td>6</td><td>Understanding the business knowledge of the industry associated with project (example RBC - Finance)</td><td>Job shadow on 1 other module of the project(Cross Team learning)</td><td>Check with Project manager for the training required to understand the industry</td></tr>'); 
+  
 });
 
-$( "#round-button-circle-Expert" ).click(function() {
+/*$( "#round-button-circle-Expert" ).click(function() {
   $('.table tr:last').after('<tr> <td> 2 </td> <td> ha </td> <td> ha </td> <td> ha </td> <td> ha </td> </tr>');
   $(this).data('clicked', true);
 });
@@ -28,9 +29,8 @@ $( "#round-button-circle-Expert" ).click(function() {
 $( "#round-button-circle-Lead" ).click(function() {
   $('.table tr:last').after('<tr> <td> 2 </td> <td> ha </td> <td> ha </td> <td> ha </td> <td> ha </td> </tr>');
   $(this).data('clicked', true);
-});
-}
-clickCircles();
+});*/
+
 
 
 
